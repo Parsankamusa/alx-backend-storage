29d9c29553ff7fae6dafa3db6fe3567a7b5f52fc
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2023-06-09 16:17:46 +0300

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,6 +12,8 @@ class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
+        """ defining a method to type-annonate
+        """
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
